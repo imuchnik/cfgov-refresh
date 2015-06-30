5ee8753e588140c3eeb0298bdc4983182f806b9f
Anselm Bradford <Anselm.Bradford@cfpb.gov>
2015-06-30 11:57:16 -0400

    code review edit

--- src/static/js/routes/the-bureau/index.js
+++ src/static/js/routes/the-bureau/index.js
@@ -5,43 +5,17 @@
 
 'use strict';
 
-var $ = require( 'jquery' );
-require( 'slick' );
-
-var BreakpointHandler = require( '../../modules/classes/BreakpointHandler' );
-var MobileOnlyExpandable = require( '../../modules/classes/MobileOnlyExpandable' );
+var MobileCarousel = require( '../../modules/classes/MobileCarousel' );
 
 function init() {
-  if ( $( 'html' ).hasClass( 'lt-ie9' ) ) {
-    return;
-  }
 
   // TODO: Remove this when per-page JS is introduced.
-  if ( document.querySelectorAll( '.bureau-mission' ).length === 0 ) {
+  if ( document.querySelectorAll( '.js-mobile-carousel' ).length === 0 ) {
     return;
   }
 
-  var bureauMissionDom = $( '.bureau-mission' );
-  var handler = new BreakpointHandler( {
-    breakpoint: 599,
-    type:       'max',
-    enter:      _enterBreakpoint,
-    leave:      _leaveBreakpoint
-  } );
-
-  $( '.expandable__mobile-only' ).each( function() {
-    handler = new MobileOnlyExpandable( $( this ), 599 );
-  } );
-
-  function _enterBreakpoint() {
-    bureauMissionDom.slick( {
-      dots: true
-    } );
-  }
-
-  function _leaveBreakpoint() {
-    bureauMissionDom.unslick();
-  }
+  var mobileCarousel = new MobileCarousel();
+  mobileCarousel.enableOn( '.js-mobile-carousel' );
 }
 
 module.exports = { init: init };
