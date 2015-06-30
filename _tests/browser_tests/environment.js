ee326c4565c94716a449aef3fb26ac736db7b223
James Wilson <Jimmy.Wilson@cfpb.gov>
2015-06-29 23:00:40 -0400

    Removed duplicate capabilities

--- _tests/browser_tests/environment.js
+++ _tests/browser_tests/environment.js
@@ -6,14 +6,6 @@ module.exports = {
   seleniumAddress:
     (process.env.SELENIUM_URL || 'http://localhost:4444/wd/hub'),
 
-  // Capabilities to be passed to the webdriver instance.
-  capabilities: {
-    'browserName':
-        (process.env.TEST_BROWSER_NAME || 'chrome'),
-    'version':
-        (process.env.TEST_BROWSER_VERSION || 'ANY')
-  },
-
   // Default http port to host the web server
   webServerDefaultPort: webServerDefaultPort,
 
